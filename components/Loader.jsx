3ce2ec01983d3280dfc5415209f1901d1344ff88
adriendelagneau <delagneauadrien@yahoo.fr>
2023-08-24 10:56:37 +0200

    first commit4

---
+++ components/Loader.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Loader = () => {
+  return (
+    <div className='absolute bottom-10 right-10'>
+          <div class="loading">
+          <div class="circle"></div>
+          <div class="circle"></div>
+          <div class="circle"></div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Loader
